1a206b02ca8f4a7aeb26191aef3f55edd0e0d193
yunsomphors20 <yunsomphors20@kit.edu.kh>
2023-01-27 02:32:51 +0700

    make referral responsive and fix search in learning

--- lessonPage/lesson.js
+++ lessonPage/lesson.js
@@ -1,7 +1,9 @@
 const courseCardTemplate = document.querySelector("[course-data-template]")
 const courseCardContainer = document.querySelector("[course-card-container]")
 const searchInput = document.querySelector("[course-search]")
-
+const dcourse = document.querySelectorAll(".course")
+// console.log(dcourse);
+// console.log(courseCardTemplate.);
 let courses = [];
 
 searchInput.addEventListener("input", e => {
@@ -9,18 +11,31 @@ searchInput.addEventListener("input", e => {
     courses.forEach(info => {
         const isVisible = 
             info.title.toLowerCase().includes(value)
-        info.element.classList.toggle("hide", !isVisible)
+        // info.element.classList.toggle("hideCourse", !isVisible)
+        if (isVisible) {
+            info.element.removeAttribute("hidden")
+        } 
+        else {
+            info.element.setAttribute("hidden", "")
+        }
     })
 })
 
 
-fetch('/lesson.json')
+fetch('lesson.json')
     .then(res => res.json()).then(data =>{
+        // console.log(data);
         courses = data.map(info => {
-            const card = courseCardTemplate.textContent.cloneNode(true).children[0]
-            const title = card.querySelector("[data-title]")
-            title.textContent = info.title
-            courseCardContainer.append(card)
+            // const id = "#"+"\\"+info.id
+            let card = null
+            dcourse.forEach(ele => {
+                if (ele.id.toString() === info.id.toString()) card = ele;  
+            })
+            
+            // console.log(card);
+            // const title = card.querySelector("[data-title]")
+            // title.textContent = info.title
+            // courseCardContainer.append(card)
             return {title: info.title, element:card}
         })
     }) 
